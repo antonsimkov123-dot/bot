8936a534c987d59a17c2e7bb937f74b6b59c24f9
ilya shimkov <antonsimkov123@gmail.com>
2025-08-20 19:55:49 +0700

    Delete utils directory

--- utils/helpers.py
+++
@@ -1,9 +0,0 @@
-"""Miscellaneous helper utilities."""
-from __future__ import annotations
-
-from datetime import datetime
-
-
-def now_str() -> str:
-    """Return current UTC time as ISO string."""
-    return datetime.utcnow().isoformat()
